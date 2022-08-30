ad07d56dc4c07280d911989dbaf38366737aaf26
Artem <slivnitsin.art@mail.ru>
2022-08-31 03:06:24 +0800

    task-25.1

---
+++ task_25metod/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int num_n=1;
+for(int i = 0; i < n; i++) 
+{
+    num_n *= num;
+}
+
+Console.Write("{0} ^ {1} = {2}", num, n, num_n);
