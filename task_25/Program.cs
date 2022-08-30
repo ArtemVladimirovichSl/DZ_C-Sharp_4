60cf170d4772b8cfd425d9fb3a39f87fcd8a2819
Artem <slivnitsin.art@mail.ru>
2022-08-31 03:05:58 +0800

    task-25

---
+++ task_25/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Math.Pow(A, B));
